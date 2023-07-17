68489dd9da376bd86782896a94c3e6f15077e491
Derek Van Vuuren <derek.vanvuuren@gmail.com>
2023-07-16 22:01:27 -0700

    removed some comments

--- script/speech.py
+++ script/speech.py
@@ -19,21 +19,8 @@ for req in import_array:
 from gtts import gTTS
 from playsound import playsound
 
-# The text that you want to convert to audio
-mytext = 'insert text here'
-  
-# Language in which you want to convert
-language = 'en'
-  
-# Passing the text and language to the engine, 
-# here we have marked slow=False. Which tells 
-# the module that the converted audio should 
-# have a high speed
+mytext = "This is a test"
+language = 'tl'
 myobj = gTTS(text=mytext, lang=language, slow=False)
-  
-# Saving the converted audio in a mp3 file named
-# welcome 
 myobj.save("welcome.mp3")
-  
-# Playing the converted file
 playsound('welcome.mp3')
